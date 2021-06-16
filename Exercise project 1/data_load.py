08af25a5ddcd8d36a38aebd6d0fb7741e2f1f2f3
LouisKamp <louiskampeskildsen99@gmail.com>
2021-06-16 11:39:36 +0200

    data load comments

--- Exercise project 1/data_load.py
+++ Exercise project 1/data_load.py
@@ -14,18 +14,32 @@ bacteriaTypes = ["Salmonella enterica", "Bacillus cereus",
 # %%
 
 def dataLoad(filename: str) -> DataFrame:
+    """Loads data from a space separated values file
+
+    Keyword arguments:
+    filename: str -- The name of the file to be loaded
+    """
+
+    # Loads data into a dataframe
     data = pd.read_csv(filename, names=dataColumn, delimiter=" ")
-    xSize, ySize = data.shape
-    truthyRows = np.ones(xSize, dtype=bool)
+
+    # Gets the number of rows in the dataframe
+    xSize = len(data)
+
+    # Creates a boolean array of the selected rows in the dataframe
+    selectedRows = np.ones(xSize, dtype=bool)
+
+    # Loops through the dataframe and selects the rows that compiles with the rules
     for i, row in data.iterrows():
         if (row['Temperature'] >= 10 and row['Temperature'] <= 60) == False:
-            truthyRows[i] = False
+            selectedRows[i] = False
             print(f"The row: {i} does not have the required tempeture range!")
         if row['Growth rate'] < 0:
-            truthyRows[i] = False
+            selectedRows[i] = False
             print(f"The row: {i} does not have a positive growth rate!")
         if row['Bacteria'] not in bacteriaValues:
-            truthyRows[i] = False
+            selectedRows[i] = False
             print(f"The row: {i} contains a not allowed bacteria!")
 
-    return data[truthyRows]
+    # Returns the only the selected rows of the data
+    return data[selectedRows]
